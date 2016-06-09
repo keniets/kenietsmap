f536079f12bbaf65c7937f86ba40f7683816cef8
Dmitry <tangar83@gmail.com>
2016-06-09 13:59:28 +0300

    Remove some trash from menuCreator

--- menuCreator.js
+++ menuCreator.js
@@ -15,14 +15,8 @@ function rewriteMaps(file, baseFolder, dirs){
     var content = fs.readFileSync(menuPath, 'utf8');
     content = content.split('<!-- Regular expression mark. Don\'t remove! -->');
 
+         writeMapItems(menuPath, content, dirs);
 
-    // fs.readFile(file, 'utf8', 'r', function (err, contents) {
-    //
-    //     text = contents.split('<!-- Regular expression mark. Don\'t remove! -->');
-    //
-        writeMapItems(menuPath, content, dirs);
-    //
-    // });
 }
 
 function recursion(path, dirs){
@@ -92,63 +86,6 @@ function writeMapItems(file, content, dirs) {
         }
     }
 }
-    // content[1] = '';
-    // fs.readdir(pathFolder, function(err, items) {
-    //     for (var i = 0; i < items.length; i++) {
-    //
-    //             content[1] +=
-    //             '\n              <ion-item class="item-icon-left" nav-clear menu-close ui-sref="app.' +  items[i].toLowerCase() + '">' + '\n' +
-    //             '                <i class="icon ion-wand"></i>' + '\n' +
-    //             '                <h2 class="menu-text map_item">' + items[i] + '</h2>' + '\n' +
-    //             '              </ion-item>' + '\n';
-    //
-    //     }
-    //
-    //     fs.writeFile(file, content[0] + '<!-- Regular expression mark. Don\'t remove! -->', 'utf8', 'w',
-    //         function() {
-    //             fs.appendFile(file, content[1], function(err){
-    //                 fs.appendFile(file, '<!-- Regular expression mark. Don\'t remove! -->' + content[2], function(err){
-    //
-    //                 });
-    //             });
-    //         });
-    //
-    // });
 
-
-
-
-
-// function append(data, pathFile){
-//
-//     fs.appendFile(pathFile, data, function(err){
-//         if (!err)
-//             console.log("appended");
-//     });
-//
-// }
-
-// function insertMap(file, dir){
-//
-//     fs.readFile(file, 'utf8', 'r', function (err, contents) {
-//
-//         var index = contents.match(/<-- Regular expression mark. Don't remove! -->/gim);
-//         console.log(index);
-//
-//     });
-// }
-//
-// function findFolders(file, baseFolder, callback) {
-//
-//     fs.readdir(baseFolder, function(err, items) {
-//         for (var i = 0; i < items.length; i++) {
-//             fs.stat(baseFolder + '/' + items[i], function(err, stats){
-//                 if (stats.isDirectory())
-//                     callback(file, items[i]);
-//             });
-//         }
-//     });
-//
-// }
 
 
